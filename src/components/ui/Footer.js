5459955eeca1404a9b9d8acef2bad4208dc5da9d
Mazed4D <playmyfiddleman@gmail.com>
2021-11-24 15:33:51 +0100

    changed footer content

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -5,9 +5,15 @@ import * as styles from './footer.module.scss';
 const Footer = () => {
 	return (
 		<div className={styles.footer}>
-			<Link href='/contact' className={styles.navlink}>
+			{/* <Link href='/contact' className={styles.navlink}>
 				Contact us
-			</Link>
+			</Link> */}
+			<p>
+				Made by{' '}
+				<a href='https://github.com/mazed4D/' className={styles.git}>
+					Milan Paunović
+				</a>
+			</p>
 		</div>
 	);
 };
