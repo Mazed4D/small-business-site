7e53556eee6602c551fdb06a279947b4281cca4e
Mazed4D <playmyfiddleman@gmail.com>
2021-11-24 20:06:37 +0100

    added media query for a future implementation of a hamburger menu

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -3,10 +3,16 @@ import * as styles from './navbar.module.scss';
 import Navlinks from './Navlinks';
 import { StaticImage } from 'gatsby-plugin-image';
 import { Link } from 'gatsby';
+import { useMediaQuery } from 'react-responsive';
 
 const Navbar = () => {
+	const isMobile = useMediaQuery({
+		query: '(max-width: 768px)',
+	});
+
 	return (
 		<div className={styles.navbar}>
+			{/* {isMobile && <SandwichMenuButton/>} */}
 			<Link href='/' className='navbar-logo'>
 				<StaticImage
 					src='../../images/logo.png'
